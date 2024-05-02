5d5ae3f7cebe0b531785be66e55815ab00087618
latricklaosiri <163001817+latricklaosiri@users.noreply.github.com>
2024-05-02 08:11:22 -0400

    Change 7

--- Code/JSCODE/testFiles/test3.js
+++ Code/JSCODE/testFiles/test3.js
@@ -1,2 +1,2 @@
-const app = new PIXI.Application(1600, 1200, { backgroundColor: 0x1099bb });
-document.body.appendChild(app.view);
+//const app = new PIXI.Application(1600, 1200, { backgroundColor: 0x1099bb });
+//document.body.appendChild(app.view);
