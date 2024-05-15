811abdc84f480b7e8b172e58b01c0407438b702a
Isaac Mills <rooster0055@protonmail.com>
2024-05-15 11:19:17 -0400

    Add djot support and switch bundler to parcel

---
+++ parcel-transformer-djot/index.js
@@ -0,0 +1,25 @@
+import { Transformer } from '@parcel/plugin';
+import { parse, renderHTML } from '@djot/djot'
+import { readFileSync } from "fs"
+
+export default new Transformer({
+  async transform({asset}) {
+    // Retrieve the asset's source code and source map.
+    let source = await asset.getCode();
+    let result = source.replace(/<djot>([\S\s]*)<\/djot>/g, (m, dep) => {
+      // Replace the original specifier with a dependency id 
+      // as a placeholder. This will be replaced later with 
+      // the final bundle URL.
+      let code = renderHTML(parse(dep));
+      return '<article class="content">' + code + '</article>';
+    });
+
+
+    // Run it through some compiler, and set the results 
+    // on the asset.
+    asset.setCode(result);
+
+    // Return the asset
+    return [asset];
+  }
+});
