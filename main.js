917171be4c70257c306b53b0559c01e7dede90d6
latricklaosiri <163001817+latricklaosiri@users.noreply.github.com>
2024-05-16 06:04:13 +0000

    bird code

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function setup(amount) {
     const birds = []
     for (var i = 0; i < amount; i++) {
         birds.push(new Bird("blue"));
+        birds.push(new Bird("red"));
     }
     return birds;
 }
@@ -17,29 +18,41 @@ function shuffle(array) {
       return array;
 }
 
-function match(birds) {
-    otherArray = []
+function contest(array) {
+    let otherArray = []
 
-    shuffle(birds)
+    array = shuffle(array)
 
-    for (let i = 0; i < birds.length; i++) {
+    for (let i = array.length-1; i >= 0; i-=2) {
         // pair up random birds to contest
-        const [a, b] = sampleSize(birds, 2);
-        a.contest(b, value, cost);
+        const [a,b] = array.splice(0,2);
+        //console.log(a.strategy);
+        a.match(b);
+        console.log(a);
+        console.log(b);
+        otherArray.push(a,b);
+        console.log(otherArray);
     }
 
     // generate next generation
-    const draw = sampleSize(birds, 1000, { weights: fitnesses });
-    return draw.map(bird => bird.spawn());
+    return otherArray;
     
   }; 
 
 function main() {
-    let birds = setup(5);
-    birds = shuffle(birds);
-    console.log(birds.length);
+    let birds = []
+    birds = setup(5);
+    /*
     console.log(birds);
-    birds = shuffle()
+    console.log(birds.length);
+    for (let i = birds.length - 1; i > 0; i--) { 
+        console.log(birds[i].strategy); 
+        console.log(birds[i].value);
+      }
+    */
+      
+    birds = contest(birds);
+    
 }
 
 main();
--- src/bird.js
+++ src/bird.js
@@ -5,19 +5,25 @@ export class Bird {
     }
     match(opponent) {
         this.value = 0;
-        if (self.strategy == "red" && opponent.strategy == "red") {
+        opponent.value = 0;
+        if (this.strategy == "red" && opponent.strategy == "red") {
             opponent.value += .75;
-            self.value += .75;
+            this.value += .75;
         }
 
-        if (self.strategy == "blue" && opponent.strategy == "blue") {
+        if (this.strategy == "blue" && opponent.strategy == "blue") {
             opponent.value += 1;
-            self.value += 1;
+            this.value += 1;
         }
 
-        if (self.strategy == "red" && opponent.strategy == "red") {
+        if (this.strategy == "blue" && opponent.strategy == "red") {
             opponent.value += 1.75
-            self.value += .25
+            this.value += .25
+        } 
+
+        if (this.strategy == "red" && opponent.strategy == "blue") {
+            opponent.value += .25
+            this.value += 1.75
         } 
     }
     toString() {
