de6bf37cc2a91594e6b8c2d714565994036ed8c9
latricklaosiri <163001817+latricklaosiri@users.noreply.github.com>
2024-05-17 06:51:53 +0000

    more bird code

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 import { Bird } from './src/bird.js';
-var brd = new Bird('Blue');
+//Carrying Capacity of Trees for birds
+let cc = 10;
+//Test Variables
+let reds = 0;
+let blues = 0;
 
-function setup(amount) {
-    const birds = []
-    for (var i = 0; i < amount; i++) {
+//Setup function for number of each bird
+function setup(blues,reds,purples,yellows) {
+    let birds = []
+    for (var i = 0; i < blues; i++) {
         birds.push(new Bird("blue"));
+    }
+    for (var i = 0; i < reds; i++) {
         birds.push(new Bird("red"));
+    }
+    for (var i = 0; i < purples; i++) {
+        birds.push(new Bird("purple"));
+    }
+    for (var i = 0; i < yellows; i++) {
+        birds.push(new Bird("yellow"));
     }
     return birds;
 }
 
+//Randomize Birds
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) { 
         const j = Math.floor(Math.random() * (i + 1)); 
@@ -18,41 +32,141 @@ function shuffle(array) {
       return array;
 }
 
+//Handles How birds reproduce
 function contest(array) {
-    let otherArray = []
+    let otherArray = [];
 
-    array = shuffle(array)
+    array = shuffle(array);
 
-    for (let i = array.length-1; i >= 0; i-=2) {
-        // pair up random birds to contest
-        const [a,b] = array.splice(0,2);
-        //console.log(a.strategy);
-        a.match(b);
-        console.log(a);
-        console.log(b);
-        otherArray.push(a,b);
-        console.log(otherArray);
-    }
-
-    // generate next generation
-    return otherArray;
+    //If bird count < carrying capacity (number of trees) = Each bird gets own tree
+    if (array.length <= cc) {
+        console.log("Default 1");
+        
+        
+        for (let i = 0; i < array.length; ++i) {
+            let a = array[i];
+            a.add();
+            otherArray.push(a);
+            
+        }
+        for (let b = 0; b < array.length; ++b) {
+            console.log("Default 1: " + otherArray[b]);
+        }
+        return otherArray;
     
+    } 
+    //If bird count < cc * 2 = There are only some birds that get their own trees - not all
+    else if (array.length < cc * 2) {
+        console.log("Default 2");
+        
+        
+        for (let i = 0; i < cc*2 - array.length; ++i) {
+            let a = array[i];
+            a.add();
+            otherArray.push(a);
+            
+        }
+        for (let b = 0; b < otherArray.length; ++b) {
+            console.log("Default 2: " + otherArray[b]);
+        }
+        
+        //Leads from earlier case - Handles normal bird Competition
+        for (let i = array.length - 1; i >= 0; i -= 2) {
+
+            if (i != 0) {
+                // pair up random birds to contest
+                const [a, b] = array.splice(0, 2);
+                //console.log(a.strategy);
+                a.match(b);
+                otherArray.push(a, b);
+            } else {
+                let a = (array[0]);
+                otherArray.push(a);
+            }
+        }
+        // generate next generation
+        return otherArray;
+        /*
+        
+        */
+        
+    }
+    else {
+        console.log("Normal Comp");
+        let overC = false; let checkC = 0;
+        for (let i = array.length - 1; i >= 0; i -= 1) {
+
+            
+                // pair up random birds to contest
+                if(checkC >= cc*2) {console.log("CC Hit");return otherArray;} 
+                checkC += 2;
+                const [a, b] = array.splice(0, 2);
+                //console.log(a.strategy);
+                a.match(b);
+                otherArray.push(a, b);
+                debugger;
+                
+            
+            
+        }
+    //Also leads from earlier case - Handles normal bird Competition while accounting for carrying capacity
+        
+    }    
   }; 
+
+//Handles bird reproduction based on value the bird has
+function reproduce(array) {
+    let otherArray = []
+    //Check value of each bird
+    for (let i = 0; i < array.length; i++) {
+        if(array[i].value >= 1) {
+    //Reproduce based on value
+        //For each whole number, add one, with percentage based on rest of decimal
+            let temp = array[i];
+            const rnd = Math.random() * 100000;
+            const percent = rnd / 1000;
+            
+            otherArray.push(new Bird(temp.strategy));
+
+            if (percent > 100 - ((temp.value-1)*100)) {
+                otherArray.push(new Bird(temp.strategy));
+            }
+
+        } else {
+        //Else, if less than one, reproduce based on that percentage
+            let temp = array[i];
+            const rnd = Math.random() * 100000;
+            const percent = rnd / 1000;
+
+            if (percent > 100 - ((temp.value-1)*100)) {
+                otherArray.push(new Bird(temp.strategy));
+            }
+        }
+    }
+    return(otherArray);
+}
 
 function main() {
     let birds = []
-    birds = setup(5);
-    /*
-    console.log(birds);
-    console.log(birds.length);
-    for (let i = birds.length - 1; i > 0; i--) { 
-        console.log(birds[i].strategy); 
-        console.log(birds[i].value);
-      }
-    */
-      
-    birds = contest(birds);
-    
-}
+    birds = setup(19, 1, 0, 0);
+    for (let i = 0; i < 10; ++i) {
+        reds = 0; blues = 0;
+        console.log("trial: " + i);
+        birds = contest(birds);
+        for (let b = 0; b < birds.length; ++b) {
+            console.log("Contest: " + birds[b]);
+            //Values should be sent from here
+        }
+        birds = reproduce(birds);
 
+        for (let b = 0; b < birds.length; ++b) {
+            console.log("Reproduce: " + birds[b]);
+            if (birds[b].strategy == "red") { ++reds; }
+            if (birds[b].strategy == "blue") { ++blues; }
+        }
+        console.log("#Reds: " + reds + " #Blues: " + blues);
+    }
+
+}
+//TO IMPLEMENT - Link to Graph
 main();
--- src/bird.js
+++ src/bird.js
@@ -1,33 +1,41 @@
 export class Bird {
+    //Simple Constructor
     constructor(strategy) {
         this.strategy = strategy;
         this.value = 0;
     }
+
+    //Match against opponent - will optimize later, also add repeated interactions (If have time?)
     match(opponent) {
         this.value = 0;
         opponent.value = 0;
         if (this.strategy == "red" && opponent.strategy == "red") {
-            opponent.value += .75;
-            this.value += .75;
+            opponent.value += .25;
+            this.value += .25;
         }
 
         if (this.strategy == "blue" && opponent.strategy == "blue") {
-            opponent.value += 1;
-            this.value += 1;
+            opponent.value += 1.25;
+            this.value += 1.25;
         }
 
         if (this.strategy == "blue" && opponent.strategy == "red") {
-            opponent.value += 1.75
-            this.value += .25
+            opponent.value += 1.5
+            this.value += .5
         } 
 
         if (this.strategy == "red" && opponent.strategy == "blue") {
-            opponent.value += .25
-            this.value += 1.75
+            opponent.value += .5
+            this.value += 1.5
         } 
     }
+    //Default
+    add() {
+        this.value += 2;
+    }
+    
     toString() {
-        return "Bird " + this.strategy;
+        return "Strategy: " + this.strategy + " Value: " + this.value;
     }
 
 }
